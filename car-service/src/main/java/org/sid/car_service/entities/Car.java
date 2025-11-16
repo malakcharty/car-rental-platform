60b11fb2a09f6350ea3a3ec5fe98352a7b775e7f
malakcharty <malakcharty1@gmail.com>
2025-11-16 22:42:40 +0100

    first commit

---
+++ car-service/src/main/java/org/sid/car_service/entities/Car.java
@@ -0,0 +1,19 @@
+package org.sid.car_service.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Car {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private double pricePerDay;
+    private boolean available;
+}
---
+++ car-service/src/main/java/org/sid/car_service/repositories/CarRepository.java
@@ -0,0 +1,8 @@
+package org.sid.car_service.repositories;
+
+import org.sid.car_service.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CarRepository extends JpaRepository<Car, Long> { }
